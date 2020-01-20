f67e05d026253e39f4639fe5d5c907d42c3b8e9b
fintan655 <g00349045@gmit.ie>
2020-01-20 17:31:49 +0000

    added for loop

--- src/ie/gmit/packagename/HelloWorld.java
+++ src/ie/gmit/packagename/HelloWorld.java
@@ -4,7 +4,9 @@ class HelloWorld {
 
     public static void main(String[] args) {
         // Print Hello World! to the console.
-        System.out.println("Hello World!");
+        System.out.println("Hello fintan!");
+   for (int i = 0; i < 10; i++) {
+      System.out.println("hello world");
+    }  
     }
-
 }
